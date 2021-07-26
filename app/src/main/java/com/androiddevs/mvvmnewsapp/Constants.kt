2d94fb3954476ef88ac3749e4f286c6312bda942
Aditya Gupta <adutyagupta765469@gmail.com>
2021-07-26 16:41:41 +0530

    added retrofit instance

--- app/src/main/java/com/androiddevs/mvvmnewsapp/Constants.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/Constants.kt
@@ -3,5 +3,6 @@ package com.androiddevs.mvvmnewsapp
 class Constants {
     companion object{
         const val API_KEY = "0ff9fc38bfd84c07a7bdcba6fc9adacf"
+        const val BASE_URL = "https://newsapi.org"
     }
 }
