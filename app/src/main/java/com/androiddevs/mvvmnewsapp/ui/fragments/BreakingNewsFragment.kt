87d1035594d180a0b2d5752e96ad41cf50bdc3d1
Aditya Gupta <adutyagupta765469@gmail.com>
2021-07-26 13:33:03 +0530

    Added BreakingNewsFragment

--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/fragments/BreakingNewsFragment.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/fragments/BreakingNewsFragment.kt
@@ -1,4 +1,7 @@
 package com.androiddevs.mvvmnewsapp.ui.fragments
 
-class BreakingNewsFragment {
+import androidx.fragment.app.Fragment
+import com.androiddevs.mvvmnewsapp.R
+
+class BreakingNewsFragment: Fragment(R.layout.fragment_breaking_news) {
 }
